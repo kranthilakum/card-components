336ea96d91d0f1006a43e65f2cded9754ed9b881
Kranthi Lakum <kranthilakum+code@gmail.com>
2019-05-22 23:44:33 +0530

    Update Card component
    Replace function with Class

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-function Card() {
-  return (
-    <div className="Card">
-      <h1>Card Title</h1>
-      <h2>Card content here!</h2>
-    </div>
-  );
+class Card extends React.Component {
+  render() {
+    return (
+      <div className="Card">
+        <h1>Card Title</h1>
+        <h2>Card content here!</h2>
+      </div>
+    );
+  }
 }
 
 const rootElement = document.getElementById("root");
