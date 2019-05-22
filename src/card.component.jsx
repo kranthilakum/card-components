32c2c530ac7cfa788be3d7eb33b1c32d14ef2939
Kranthi Lakum <kranthilakum+code@gmail.com>
2019-05-22 23:50:43 +0530

    Move Card component to separate file

---
+++ src/card.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class Card extends React.Component {
+  render() {
+    return (
+      <div className="Card">
+        <h1>Card Title</h1>
+        <h2>Card content here!</h2>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import Card from "./card.component";
 import "./styles.css";
-
-class Card extends React.Component {
-  render() {
-    return (
-      <div className="Card">
-        <h1>Card Title</h1>
-        <h2>Card content here!</h2>
-      </div>
-    );
-  }
-}
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<Card />, rootElement);
