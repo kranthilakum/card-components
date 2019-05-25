13f442c9fa9a50117a6fcba2404401afabe2e55f
Kranthi Lakum <kranthilakum+code@gmail.com>
2019-05-25 19:02:32 +0530

    Add tailwindcss
    Add CSS to components

--- src/card-header.component.jsx
+++ src/card-header.component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default class CardHeader extends React.Component {
   render() {
     return (
-      <div className="card-header">
+      <div className="font-bold text-xl px-6 py-4">
         <span>{this.props.text}</span>
       </div>
     );
--- src/card-image.component.jsx
+++ src/card-image.component.jsx
@@ -4,12 +4,10 @@ export default class CardImage extends React.Component {
   render() {
     return (
       <div className="card-image-container">
-        <img
-          className="card-image"
-          src={this.props.url}
-          alt={this.props.name}
-        />
-        <h4>{this.props.caption}</h4>
+        <img className="w-full" src={this.props.url} alt={this.props.name} />
+        <p className="text-sm text-gray-600 text-center">
+          {this.props.caption}
+        </p>
       </div>
     );
   }
